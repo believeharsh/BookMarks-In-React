0844ea80612e5ab7a1dcf55fa5399ad7a9d00dea
Believeharsh <bontinue@gmail.com>
2024-02-02 17:40:56 +0530

    added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
-    <div className="text-black">
-    harsh Dahiya 
-      
-    </div>
+<>
+  <Header/>
+</>
   )
 }
 
