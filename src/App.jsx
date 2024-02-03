17ef089efae90f316fdc6c7ab76ead7734894d0e
Believeharsh <bontinue@gmail.com>
2024-02-03 22:53:21 +0530

    formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from './Components/Header/Header'
+import React from "react";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-<>
-  <Header/>
-</>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
