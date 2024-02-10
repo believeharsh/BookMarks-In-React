662f22b9d8133b73764c3784aee9d0d2951f30b4
Believeharsh <bontinue@gmail.com>
2024-02-10 11:57:51 +0530

    formated

---
+++ src/components/EditNote.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const EditNote = () => {
+  return (
+    <div
+    className={`w-full max-w-xl z-[15] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] `}
+  >
+    <div className="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          value={noteText}  onChange={handleChange}
+          ref={nameRef}
+         
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Url
+        </label>
+        <input
+          className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="text"
+          placeholder="https://Github.com"
+          value={noteurl}
+          onChange={e => setnoteurl(e.target.value)}
+     
+        
+         
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          onClick={handleSaveClick}
+        >
+          Save
+        </button>
+        <button
+          onClick={HandleCloseNoteDetail}
+          className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+        >
+          Cancle
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default EditNote
--- src/components/NoteMenu.jsx
+++ src/components/NoteMenu.jsx
@@ -4,7 +4,7 @@ const NoteMenu = ({ OpenMenu, handleDeleteNote, BM , HandleOpneNoteDetail}) => {
   return (
     <div className={`w-[100px] h-[60px]  bg-black rounded-xl py-2 m-1 ${OpenMenu ? 'visible' : 'hidden'}`}>
       <div className="flex flex-col justify-center items-center   text-white ">
-      <div className='bg-transparent  py-1 hover:bg-gray-500 w-full text-center' onclick={() => HandleOpneNoteDetail}>Edit Note</div>
+      <div className='bg-transparent  py-1 hover:bg-gray-500 w-full text-center' >Edit Note</div>
       <div className='bg-transparent  py-1 hover:bg-gray-500 w-full text-center' onClick={() => handleDeleteNote(BM.id)} >Remove</div>
       </div>
     </div>
