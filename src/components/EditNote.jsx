d28edd6fc1d95400b5f52dbada698ca656994557
Believeharsh <bontinue@gmail.com>
2024-02-18 14:34:15 +0530

    formated

--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -45,7 +45,7 @@ const EditNote = () => {
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
-          onClick={handleSaveClick}
+          // onClick={handleSaveClick}
         >
           Save
         </button>
