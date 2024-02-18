5fbce35cba50e2a0e52b83c9d5b6e846066d2815
Believeharsh <bontinue@gmail.com>
2024-02-18 11:49:07 +0530

    formated

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,6 @@
 import { CiMenuKebab } from "react-icons/ci";
 import NoteMenu from "./NoteMenu";
 
-
 const Note = ({
   BM,
   handleDeleteNote,
@@ -10,11 +9,15 @@ const Note = ({
   HandleOpneNoteDetail,
 }) => {
   return (
-    <div className="group Container bg-tranparent  hover:border-[1px] mx-1 my-1 px-1 py-2 rounded-xl w-[90px] h-[80px] hover:bg-gray-400 hover:border-black   hover:font-thin relative">
-
-<div className="hidden group-hover:block group-hover:text-2xl text-black text-[5px] absolute right-[3px] top-[3px] cursor-pointer" onClick={handleOpenMenu}>
-  <p><CiMenuKebab /></p>
-</div>
+    <div className="group Container bg-tranparent bg-blend-multiply hover:border-[1px] mx-1 my-1 px-1 py-2 rounded-xl w-[90px] h-[80px] hover:bg-gray-600 hover:border-black   hover:font-thin relative">
+      <div
+        className="hidden group-hover:block group-hover:text-2xl text-black text-[5px] absolute right-[3px] top-[3px] cursor-pointer"
+        onClick={handleOpenMenu}
+      >
+        <p>
+          <CiMenuKebab />
+        </p>
+      </div>
 
       <div className="">
         <div className="bg-gray-800 py-3 rounded-full my-1 h-[45px] w-[45px] flex justify-center items-center mx-auto">
@@ -29,29 +32,22 @@ const Note = ({
               alt=""
             />
           </a>
-
-        
         </div>
 
         <p className="text-center mb-4">{BM.name}</p>
       </div>
-     {
-      OpenMenu ? 
-      <div className="absolute top-6 left-0 right-0">
-        <NoteMenu
-          OpenMenu={OpenMenu}
-          handleDeleteNote={handleDeleteNote}
-          BM={BM}
-          HandleOpneNoteDetail={HandleOpneNoteDetail}
-        />
-      </div>
-      : 
-      <div className="">
-
-      </div>
-
-     }
-     
+      {OpenMenu ? (
+        <div className="absolute top-6 left-0 right-0">
+          <NoteMenu
+            OpenMenu={OpenMenu}
+            handleDeleteNote={handleDeleteNote}
+            BM={BM}
+            HandleOpneNoteDetail={HandleOpneNoteDetail}
+          />
+        </div>
+      ) : (
+        <div className=""></div>
+      )}
     </div>
   );
 };
